d0e35a8cb5cc5cabbfcf8e5d1a3d383e2e69fab7
carlasanjose <98812058+carlasanjose@users.noreply.github.com>
2022-04-25 11:11:40 +0200

    Add files via upload

---
+++ lineas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: Lasjimenez
+"""
+
+import random
+
+archivo_entrada = open('quijote.txt', 'r')
+archivo_salida = open('quijote_s05.txt', 'w')
+p = random.random()
+for linea in archivo_entrada:
+    d = random.random()
+    if d <= p:
+        archivo_salida.write(linea)
+archivo_entrada.close()
+archivo_salida.close()
